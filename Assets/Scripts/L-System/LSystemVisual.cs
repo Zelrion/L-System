215e28ffac4187101ec0ad30b960f04a38f028ab
Gavin McDowell <mcdowgav@gmail.com>
2018-02-05 18:09:45 -0800

    Basic functionality

---
+++ Assets/Scripts/L-System/LSystemVisual.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSystemVisual:MonoBehaviour {
+	public struct State {
+		public Vector3 pos;
+		public Quaternion rot;
+	}
+	public string command;
+	public float length, angle;
+	Stack<State> stateStack;
+	void Start() {
+		stateStack = new Stack<State>();
+		command = StringGenerator.GenerateCommand();
+		Generate();
+	}
+	public void Generate() { 
+		foreach(char c in command) {
+			switch(c) {
+				case 'F':
+					Vector3 pos = transform.position;
+					transform.Translate(Vector3.forward*length);
+					Debug.DrawLine(pos,transform.position,Color.green,10000f,false);
+					break;
+				case 'f':
+					transform.Translate(Vector3.forward * length);
+					break;
+				case '+':
+					transform.Rotate(Vector3.up,-angle);
+					break;
+				case '-':
+					transform.Rotate(Vector3.up,angle);
+					break;
+				case '[':
+					stateStack.Push(new State {
+						pos = transform.position,
+						rot = transform.rotation
+					});
+					break;
+				case ']':
+					State prevST = stateStack.Pop();
+					transform.position = prevST.pos;
+					transform.rotation = prevST.rot;
+					break;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/L-System/StringGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+public static class StringGenerator {
+	private static string axiom = "X";
+	private static int iterations = 6;
+	private static Dictionary<char,string> rules = new Dictionary<char,string> {
+		{ 'X',"F[-X][X]F[-X]+FX" },
+		{ 'F',"FF" }
+	};
+	public static string GenerateCommand() {
+		string sentence = axiom;
+		for(int i = 0;i < iterations;i++) {
+			string newSentence = "";
+			foreach(char c in sentence.ToCharArray()) {
+				if(rules.ContainsKey(c)) newSentence += rules[c];
+				else newSentence += c.ToString();
+			}
+			sentence = newSentence;
+		}
+		return sentence;
+	}
+}
